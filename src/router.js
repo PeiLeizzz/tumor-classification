dff38c6f6b3c8906068f0e27ac8eb3d08b4f2b2c
peilei722 <714838284@qq.com>
2021-07-18 22:46:29 +0800

    login

--- src/router.js
+++ src/router.js
@@ -1,27 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Main from './components/MainPage.vue'
+import Login from './components/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
-    }
+      path: '/main',
+      name: 'main',
+      component: Main
+    },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/') {
+    next();
+  } else {
+    let token = localStorage.getItem('token');
+    if (token === null || token === '') {
+      next('/');
+    } else {
+      next();
+    }
+  }
+})
+
+export default router;
--- src/store.js
+++ src/store.js
@@ -4,9 +4,25 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        server_url: "http://120.55.48.190:8000"
-    },
-    mutations: {},
-    actions: {}
+	state: {
+		server_url: "http://120.55.48.190:8000",
+		token: localStorage.getItem('token') ? localStorage.getItem('token'):'',   // token
+	},
+	getters:{    // 监听数据变化的
+		getStorage(state){   // 获取本地存储的登录信息
+			if(!state.token){
+				state.token = JSON.parse(localStorage.getItem(key))
+			}
+			return state.token
+		}
+	},
+	mutations:{
+		$_setToken(state, value) { // 设置存储token
+			state.token = value;
+			localStorage.setItem('token', value);
+		},
+		$_removeStorage(state, value){  // 删除token
+			localStorage.removeItem('token');
+		}
+	}
 })
