b940ec13c72cb82d2c41e2615485f80f7c1fd671
PeiLeizzz <714838284@qq.com>
2021-07-27 19:51:43 +0800

    播放器换成video标签

--- src/components/VideoSelector.vue
+++ src/components/VideoSelector.vue
@@ -37,17 +37,9 @@
                     </a-row>
                     <a-row type="flex" justify="space-around" align="middle" v-viewer="{}">
                         <a-col style="width: 768px;height: 432px;display:flex;justify-content:center;align-content: center">
-                            <div class="my_video">
-                                <video-player class="video-player vjs-custom-skin" ref="videoPlayer" :playsinline="true" :options="options"
-                                              @play="onPlayerPlay($event)" @pause="onPlayerPause($event)" @ended="onPlayerEnded($event)"
-                                              @waiting="onPlayerWaiting($event)"
-                                              @playing="onPlayerPlaying($event)" @loadeddata="onPlayerLoadeddata($event)"
-                                              @timeupdate="onPlayerTimeupdate($event)"
-                                              @canplay="onPlayerCanplay($event)" @canplaythrough="onPlayerCanplaythrough($event)"
-                                              @statechanged="playerStateChanged($event)"
-                                              @ready="playerReadied">
-                                </video-player>
-                            </div>
+													<video id="videoPlayer" class="my_video" controls>
+														<source :src="videoUrl" type="video/mp4" />
+													</video>
                         </a-col>
                     </a-row>
                     <a-button-group style="display:flex;justify-content:space-around;margin-top:30px;">
@@ -99,11 +91,13 @@ import 'viewerjs/dist/viewer.css'
 import {directive as viewer} from "v-viewer"
 import axios from "axios";
 import Timer from "@/components/Timer"
+import Xgplayer from 'xgplayer-vue';
 
 export default {
     name: "VideoSelector",
     components: {
-        Timer
+        Timer,
+				Xgplayer
     },
     directives: {
         viewer: viewer({
@@ -169,27 +163,7 @@ export default {
             // 分类类别
             classes: [],
             url: "",
-            options: {
-                playbackRates: [0.5, 1.0, 1.5, 2.0, 3.0], // 可选的播放速度
-                autoplay: false, // 是否自动播放
-                muted: false, // 是否静音
-                loop: true, // 是否开启循环播放
-                preload: "auto", // 自动预加载
-                language: "zh-CN", // 语言，'en', 'zh-cn', 'zh-tw'
-                aspectRatio: "16:9", // 播放器高宽占比（例如"16:9"或"4:3"）
-                fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-                sources: [{
-                    type: "video/mp4", // 类型
-                    src: "" // url地址
-                }],
-                notSupportedMessage: "此视频暂无法播放，请稍后再试", // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
-                controlBar: {
-                    timeDivider: true, // 是否显示当前时间和持续时间的分隔符，"/"
-                    durationDisplay: true, // 是否显示持续时间
-                    remainingTimeDisplay: false, // 是否显示剩余时间
-                    fullscreenToggle: true, // 是否显示全屏按钮
-                }
-            }
+						videoUrl: ""
         };
     },
     methods: {
@@ -249,11 +223,10 @@ export default {
                 is_classified: []
             }
 						this.$store.commit('$_setCurrentCount', 0);
-            this.current_class = []
-            this.options.sources.src = [{
-                type: "video/mp4", // 类型
-                src: "", // url地址
-            }]
+            this.current_class = [];
+						this.videoUrl = "";
+						let video = document.getElementById("videoPlayer");
+						video.src = "";
         },
         generatePostData() {
             this.$set(this.post_data, "video_name", this.received_data.video_id[this.currentVideoIdx]);
@@ -298,7 +271,7 @@ export default {
             }
             // 如果当前有对该视频的标注
             if (this.received_data.is_classified[this.currentVideoIdx]) {
-                let _this = this;
+								let _this = this;
                 this.$confirm({
                     title: '确定要切换吗',
                     content: '视频标注每次提交一个，如果切换视频，会导致当前视频的标注清空',
@@ -341,56 +314,14 @@ export default {
             }
         },
         updateVideoUrl(id = 0) {
-            this.options.sources[0].src = this.received_data.video_url[id];
-        },
-        // 播放回调
-        onPlayerPlay($event) {
-            //console.log("player play!", $event);
-        },
-        // 暂停回调
-        onPlayerPause($event) {
-            //console.log("player pause!", $event);
-        },
-        // 视频播完回调
-        onPlayerEnded($event) {
-            //console.log($event);
-        },
-        // DOM元素上的readyState更改导致播放停止
-        onPlayerWaiting($event) {
-            //console.log($event);
-        },
-        // 已开始播放回调
-        onPlayerPlaying($event) {
-            //console.log($event);
-        },
-        // 当播放器在当前播放位置下载数据时触发
-        onPlayerLoadeddata($event) {
-            //console.log($event);
-        },
-        // 当前播放位置发生变化时触发。
-        onPlayerTimeupdate($event) {
-            //console.log($event);
-        },
-        //媒体的readyState为HAVE_FUTURE_DATA或更高
-        onPlayerCanplay($event) {
-            // console.log('player Canplay!', player)
-        },
-        //媒体的readyState为HAVE_ENOUGH_DATA或更高。这意味着可以在不缓冲的情况下播放整个媒体文件。
-        onPlayerCanplaythrough($event) {
-            // console.log('player Canplaythrough!', player)
-        },
-        //播放状态改变回调
-        playerStateChanged($event) {
-            //console.log("player current update state", $event);
-        },
-        //将侦听器绑定到组件的就绪状态。与事件监听器的不同之处在于，如果ready事件已经发生，它将立即触发该函数。。
-        playerReadied() {
-            //console.log("example player 1 readied");
+					let video = document.getElementById("videoPlayer");
+					video.src = this.received_data.video_url[id];
+					this.videoUrl = this.received_data.video_url[id];
         },
     },
     watch: {
         currentVideoIdx() {
-            this.updateVideoUrl(this.currentVideoIdx);
+          this.updateVideoUrl(this.currentVideoIdx);
         }
     }
 }
